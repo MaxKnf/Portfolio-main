dc05a67045a5b2bc3eaedc1e66f543ccbae551c2
Efilrofzaggin4 <maxime.kiniffo@3wa.io>
2024-10-23 06:51:58 +0200

    horizon studio project added

--- src/component/MyAvatar.jsx
+++ src/component/MyAvatar.jsx
@@ -1,36 +1,39 @@
-import React, { useState, useEffect, useRef } from 'react'
-import Avatar from '@mui/material/Avatar';
+import React, { useState, useEffect, useRef } from "react";
+import Avatar from "@mui/material/Avatar";
 
 function MyAvatar() {
-
   // const [isVisible, setIsVisible] = useState(false);
   //   const ref = useRef(null);
-  
-    // useEffect(() => {
-    //   const observer = new IntersectionObserver(
-    //     ([entry]) => {
-    //       setIsVisible(entry.isIntersecting);
-    //     },
-    //     {
-    //       threshold: 0
-    //     }
-    //   );
-    //   if (ref.current) {
-    //     observer.observe(ref.current);
-    //   }
-    //   return () => {
-    //     if (ref.current) {
-    //       observer.unobserve(ref.current);
-    //     }
-    //   };
-    // }, []);
+
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver(
+  //     ([entry]) => {
+  //       setIsVisible(entry.isIntersecting);
+  //     },
+  //     {
+  //       threshold: 0
+  //     }
+  //   );
+  //   if (ref.current) {
+  //     observer.observe(ref.current);
+  //   }
+  //   return () => {
+  //     if (ref.current) {
+  //       observer.unobserve(ref.current);
+  //     }
+  //   };
+  // }, []);
 
   return (
     <div>
       <a href="#about-me" aria-hidden="true" id="about-me"></a>
-        <Avatar  alt="Maxime Kiniffo" src="https://i.ibb.co/DMCqjRq/moi-cv2.jpg" sx={{marginTop:"100px", width: 250, height: 250}}/>
+      <Avatar
+        alt="Maxime Kiniffo"
+        src="https://i.ibb.co/Wx73r1h/moi-cv2.jpg"
+        sx={{ marginTop: "100px", width: 250, height: 250 }}
+      />
     </div>
-  )
+  );
 }
 
-export default MyAvatar
+export default MyAvatar;
--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -1,19 +1,17 @@
-import React, { useState, useEffect, useRef } from 'react'
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LanguageIcon from '@mui/icons-material/Language';
-
-
+import React, { useState, useEffect, useRef } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LanguageIcon from "@mui/icons-material/Language";
 
 function createData(nom, description, language, link, githubIcon, websiteIcon) {
-  return { nom, description, language,link, githubIcon, websiteIcon};
+  return { nom, description, language, link, githubIcon, websiteIcon };
 }
 
 /*
@@ -27,124 +25,181 @@ createData(
     }, 
     <GitHubIcon />, 
     <LanguageIcon />
-)
+),
 */
 
 const rows = [
   createData(
-    'Sakura Wave', 
-    "Site vitrine d'un restaurant japonais", 
-    "Javascript, React.js", 
-    {
-        github:"https://github.com/MaxKnf/sakura-wave",
-        website: "https://sakura-wave-food.netlify.app/"
-    }, 
-    <GitHubIcon />, 
-    <LanguageIcon />
-    ),
-    createData(
-    'PORTFOLIO', 
-    "mon portfolio", 
-    "Javascript, React.js, MaterialUI, W3.CSS", 
-    {
-        github:"https://github.com/MaxKnf/Portfolio-main",
-        website: null
-    }, 
-    <GitHubIcon />, 
-    null
-    ),
-    createData(
-    'ESOTALES-V2', 
-    "V2 de mon projet de fin d'année à la 3WA (projet personnel) - fake database avec des composants react", 
+    "Horizon Studio",
+    "Site vitrine d'une agence de design d'intérieur",
     "Javascript, React.js, MaterialUI",
-    { 
-        github:"https://github.com/MaxKnf/Esotales_main",  
-        website: "https://esotales-maxime-kiniffo.netlify.app/"
-    } , 
-    <GitHubIcon />, 
+    {
+      github: "https://github.com/MaxKnf/Horizon-Studio",
+      website: "https://horizon-studio.netlify.app/",
+    },
+    <GitHubIcon />,
     <LanguageIcon />
-    ),
-    createData(
-    'POKEDEX', 
-    "pokedex (encyclopédie pokemon) avec appel API", 
+  ),
+  createData(
+    "Sakura Wave",
+    "Site vitrine d'un restaurant japonais",
+    "Javascript, React.js",
+    {
+      github: "https://github.com/MaxKnf/sakura-wave",
+      website: "https://sakura-wave-food.netlify.app/",
+    },
+    <GitHubIcon />,
+    <LanguageIcon />
+  ),
+  createData(
+    "PORTFOLIO",
+    "mon portfolio",
+    "Javascript, React.js, MaterialUI, W3.CSS",
+    {
+      github: "https://github.com/MaxKnf/Portfolio-main",
+      website: null,
+    },
+    <GitHubIcon />,
+    null
+  ),
+  createData(
+    "ESOTALES-V2",
+    "V2 de mon projet de fin d'année à la 3WA (projet personnel) - fake database avec des composants react",
+    "Javascript, React.js, MaterialUI",
+    {
+      github: "https://github.com/MaxKnf/Esotales_main",
+      website: "https://esotales-maxime-kiniffo.netlify.app/",
+    },
+    <GitHubIcon />,
+    <LanguageIcon />
+  ),
+  createData(
+    "POKEDEX",
+    "pokedex (encyclopédie pokemon) avec appel API",
     "Javascript, React.js,  W3.CSS",
-    { 
-        github:"https://github.com/MaxKnf/pokedex-react",  
-        website: "https://pokedex-maxime-kiniffo.netlify.app/"
-    } , 
-    <GitHubIcon />, 
+    {
+      github: "https://github.com/MaxKnf/pokedex-react",
+      website: "https://pokedex-maxime-kiniffo.netlify.app/",
+    },
+    <GitHubIcon />,
     <LanguageIcon />
-    ),
-    createData(
-        'MERN CRUD', 
-        "Apprentissage de MongoDB et création d'un crud complet", 
-        "Javascript, React.js, MongoDB",
-        { 
-            github:"https://github.com/MaxKnf/Mern-CRUD",  
-            website: null
-        } , 
-        <GitHubIcon />,
-        null
-    ),
-    createData(
-        'ESOTALES_V1', 
-        "Projet de fin d'année pour la 3wa", 
-        "Javascript, node.js, express.js, mySQL, phpmyadmin", 
-        {
-            github:"https://github.com/MaxKnf/Esotales_3WA_projet_final",
-            website: null
-        }, 
-        <GitHubIcon />, 
-        null
-    )
+  ),
+  createData(
+    "MERN CRUD",
+    "Apprentissage de MongoDB et création d'un crud complet",
+    "Javascript, React.js, MongoDB",
+    {
+      github: "https://github.com/MaxKnf/Mern-CRUD",
+      website: null,
+    },
+    <GitHubIcon />,
+    null
+  ),
+  createData(
+    "ESOTALES_V1",
+    "Projet de fin d'année pour la 3wa",
+    "Javascript, node.js, express.js, mySQL, phpmyadmin",
+    {
+      github: "https://github.com/MaxKnf/Esotales_3WA_projet_final",
+      website: null,
+    },
+    <GitHubIcon />,
+    null
+  ),
 ];
 
 export default function Projects() {
-
-   
-
-    const theme = createTheme({
-        typography: {
-          fontFamily: [
-            'monospace',
-          ].join(','),
-        },
-      });
+  const theme = createTheme({
+    typography: {
+      fontFamily: ["monospace"].join(","),
+    },
+  });
 
   return (
-    <ThemeProvider  theme={theme}>
-    <div   style={{ width: '90%',marginTop: "80px 0", display:"flex", flexDirection:"column", alignItems: "center", color:"white"}}>
-    <a href="#projects" aria-hidden="true" id="projects"></a>
-        <h2   style={{margin: "100px 0", fontFamily:"monospace", fontSize:"3rem"}}>&#123; Projets &#125;</h2>
-        <a  href="https://github.com/MaxKnf?tab=repositories" target="_blank"><GitHubIcon sx={{width:"100px", height:"100px", borderRadius:"100px" }}/></a>
-        <TableContainer  component={Paper} sx={{marginTop: "50px", backgroundColor: "#212121",borderRadius: "10px"}}>
-        <Table  sx={{ width: "100%", backgroundColor: "#212121",}} aria-label="simple table">
+    <ThemeProvider theme={theme}>
+      <div
+        style={{
+          width: "90%",
+          marginTop: "80px 0",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          color: "white",
+        }}
+      >
+        <a href="#projects" aria-hidden="true" id="projects"></a>
+        <h2
+          style={{
+            margin: "100px 0",
+            fontFamily: "monospace",
+            fontSize: "3rem",
+          }}
+        >
+          &#123; Projets &#125;
+        </h2>
+        <a href="https://github.com/MaxKnf?tab=repositories" target="_blank">
+          <GitHubIcon
+            sx={{ width: "100px", height: "100px", borderRadius: "100px" }}
+          />
+        </a>
+        <TableContainer
+          component={Paper}
+          sx={{
+            marginTop: "50px",
+            backgroundColor: "#212121",
+            borderRadius: "10px",
+          }}
+        >
+          <Table
+            sx={{ width: "100%", backgroundColor: "#212121" }}
+            aria-label="simple table"
+          >
             <TableHead>
-            <TableRow >
-                <TableCell sx={{ color:"white", fontSize: "1.1rem" }} ><strong>NOM</strong></TableCell>
-                <TableCell sx={{ color:"white", fontSize: "1.1rem" }} ><strong>DESCRIPTION</strong></TableCell>
-                <TableCell sx={{ color:"white", fontSize: "1.1rem" }} ><strong>LANGUAGE</strong></TableCell>
-                <TableCell sx={{ color:"white", fontSize: "1.1rem" }} ><strong>LIEN</strong></TableCell>
-            </TableRow>
+              <TableRow>
+                <TableCell sx={{ color: "white", fontSize: "1.1rem" }}>
+                  <strong>NOM</strong>
+                </TableCell>
+                <TableCell sx={{ color: "white", fontSize: "1.1rem" }}>
+                  <strong>DESCRIPTION</strong>
+                </TableCell>
+                <TableCell sx={{ color: "white", fontSize: "1.1rem" }}>
+                  <strong>LANGUAGE</strong>
+                </TableCell>
+                <TableCell sx={{ color: "white", fontSize: "1.1rem" }}>
+                  <strong>LIEN</strong>
+                </TableCell>
+              </TableRow>
             </TableHead>
             <TableBody>
-            {rows.map((row) => (
+              {rows.map((row) => (
                 <TableRow
-                key={row.nom}
-                sx={{  borderTop: "2px solid rgba(224, 224, 224, 1)", borderBottom: "1px solid rgba(33, 33, 33, 1)"}}
+                  key={row.nom}
+                  sx={{
+                    borderTop: "2px solid rgba(224, 224, 224, 1)",
+                    borderBottom: "1px solid rgba(33, 33, 33, 1)",
+                  }}
                 >
-                <TableCell component="th" scope="row" sx={{ color:"white"}}>
+                  <TableCell component="th" scope="row" sx={{ color: "white" }}>
                     {row.nom}
-                </TableCell>
-                <TableCell sx={{ color:"white" }} >{row.description}</TableCell>
-                <TableCell sx={{ color:"white" }} >{row.language}</TableCell>
-                <TableCell sx={{ color:"white"}} ><a href={row.link.github} target="_blank">{row.githubIcon}</a> <a href={row.link.website} target="_blank">{row.websiteIcon}</a></TableCell>
+                  </TableCell>
+                  <TableCell sx={{ color: "white" }}>
+                    {row.description}
+                  </TableCell>
+                  <TableCell sx={{ color: "white" }}>{row.language}</TableCell>
+                  <TableCell sx={{ color: "white" }}>
+                    <a href={row.link.github} target="_blank">
+                      {row.githubIcon}
+                    </a>{" "}
+                    <a href={row.link.website} target="_blank">
+                      {row.websiteIcon}
+                    </a>
+                  </TableCell>
                 </TableRow>
-            ))}
+              ))}
             </TableBody>
-        </Table>
+          </Table>
         </TableContainer>
-    </div>
+      </div>
     </ThemeProvider>
   );
 }
